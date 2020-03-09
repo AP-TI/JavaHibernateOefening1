db5875e2424b6031ac8b90c1f2b1e7db61aa597a
mixxamm <maximjanssensj5@gmail.com>
2020-03-09 20:45:55 +0100

    Maak oefening verder af
    Oké output is nog wel lelijk, maar doet al wat de opgave vraagt.

--- src/main/java/be/apti/Main.java
+++ src/main/java/be/apti/Main.java
@@ -9,21 +9,27 @@ import org.hibernate.query.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Root;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try{
             SessionFactory factory = new Configuration().configure().buildSessionFactory();
             Session session = factory.openSession();
-
             try{
+                Scanner scanner = new Scanner(System.in);
                 CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
                 CriteriaQuery<Lector> criteriaQuery = criteriaBuilder.createQuery(Lector.class);
                 Root<Lector> root = criteriaQuery.from(Lector.class);
-                CriteriaQuery<Lector> alles = criteriaQuery.select(root);
-                TypedQuery<Lector> typedQuery = session.createQuery(alles);
+                System.out.print("Geef lector naam: ");
+                String lectorNaam = scanner.nextLine();
+                System.out.println("Gekozen lector: " + lectorNaam);
+                criteriaQuery.select(root);
+                criteriaQuery.where(criteriaBuilder.equal(root.get("naam"), lectorNaam));
+                TypedQuery<Lector> typedQuery = session.createQuery(criteriaQuery);
                 List<Lector> lectorList = typedQuery.getResultList();
                 for(Lector lector : lectorList){
                     System.out.println(lector.toString());
